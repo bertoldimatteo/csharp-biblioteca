509a2846a38f42d71e2aa253cb3f1d8629631c56
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-20 15:01:33 +0200

    sposto la classe user nel file dedicato

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class User
-{
-    string name;
-    string surname;
-    string email;
-    string password;
-    int mobilePhone;
-}
+
---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    class User
+    {
+        string name;
+        string surname;
+        string email;
+        string password;
+        int mobilePhone;
+    }
+}
