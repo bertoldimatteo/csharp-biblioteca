482ab61cc8209d514c27e5f30d486132a66e4fcd
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-20 14:33:20 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class User
+{
+    string name;
+    string surname;
+    string email;
+    string password;
+    int mobilePhone;
+}
